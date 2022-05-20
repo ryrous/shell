bcdd5811eb7750c9dea8cc930dd98fde452682d6
Your Name <you@example.com>
2022-05-20 07:42:50 -1000

    shell

---
+++ AnsibleSSH4Hosts.sh
@@ -0,0 +1,17 @@
+# create password text file
+echo password > password.txt
+
+# establish ssh with multiple hosts
+for user in ansible root
+> do
+>  for os in ubuntu centos
+>  do
+>   for instance in 1 2 3
+>   do
+>    sshpass -f password.txt ssh-copy-id -o StrictHostKeyChecking=no ${user}@${os}${instance} 
+>   done
+>  done
+> done
+
+# cleanup for security if needed
+rm password.txt
