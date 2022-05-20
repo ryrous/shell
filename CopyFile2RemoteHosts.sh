e458c3aaf5aeaee02c7134bed8598ef083dadc04
Your Name <you@example.com>
2022-05-20 07:45:52 -1000

    shell

--- CopyFile2RemoteHosts.sh
+++ CopyFile2RemoteHosts.sh
@@ -1,4 +1,4 @@
 # install pssh as needed
 sudo yum install pssh
 # copy
-pscp -A -h hostnames.txt -l username -pw password filename.ext /tmp/ -p 5 -e /tmp
+pscp -A -h hostnames.txt -l username -pw password filename.ext /tmp -p 5 -e /tmp
