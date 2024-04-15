63172f11f32bd727360d697cfaf9f145170e7670
ryrous <38826351+ryrous@users.noreply.github.com>
2024-04-15 15:01:07 -0400

    shell

---
+++ InstallVSCode4Debian.sh
@@ -0,0 +1,17 @@
+# Check for root privileges
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root (use sudo)" 
+   exit 1
+fi
+
+# The repository and key can be installed manually with the following script:
+apt-get install wget gpg
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+apt install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+rm -f packages.microsoft.gpg
+
+# Then update the package cache and install the package using:
+apt install apt-transport-https
+apt update && apt upgrade -y
+apt install code # or code-insiders
