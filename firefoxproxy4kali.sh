c6346bb523339f20297606efc1bd98962ac38369
ryrous <38826351+ryrous@users.noreply.github.com>
2024-04-24 10:36:43 -0400

    shell

--- firefoxproxy4kali.sh
+++ firefoxproxy4kali.sh
@@ -1,7 +1,7 @@
 # Edit proxychains.conf:
 sudo mousepad /etc/proxychains4.conf
 # Uncomment "strict_chain" and "proxy_dns"
-# Uncomment the last line of the file to look like this:
+# Edit the "ProxyList" to look like this:
 # socks5 127.0.0.1 9050
 # Save and close the file
 
