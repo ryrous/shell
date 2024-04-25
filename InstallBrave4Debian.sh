a3ac77ea7769c930913dee8ca87501c3185535bb
ryrous <38826351+ryrous@users.noreply.github.com>
2024-04-24 20:35:35 -0400

    shell

---
+++ InstallBrave4Debian.sh
@@ -0,0 +1,10 @@
+# Install curl if you don't have it
+sudo apt install curl
+# curl the keyring
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+# echo and tee
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+# Update
+sudo apt update
+# Install Brave Browser
+sudo apt install brave-browser
